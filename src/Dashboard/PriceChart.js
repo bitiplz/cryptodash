2a90e3186a436a217611cb51c4cf2e0fcebb6270
bitiplz <uberud16@gmail.com>
2020-01-04 21:11:51 +0100

    highcharts theme

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -1,8 +1,11 @@
 import highchartsConfig from './HighchartsConfig';
+import highchartsTheme from './HighchartsTheme';
 import React from 'react';
 import {Tile} from "../Shared/Tile";
 import {AppContext} from "../App/AppProvider";
 import ReactHighCharts from 'react-highcharts';
+
+ReactHighCharts.Highcharts.setOptions(highchartsTheme);
 
 export default function(){
     return (
